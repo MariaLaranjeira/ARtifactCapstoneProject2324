0728b5bc0629bf729b67f2760fe2e5cac253af52
LeM0n17 <93678059+LeM0n17@users.noreply.github.com>
2024-06-07 03:22:01 +0100

    added some fixes

---
+++ Assets/Scripts/puzzleScripts/showShare.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showShare : MonoBehaviour
+{
+    public GameObject sharePanel;
+    public void showSharePanel()
+    {
+        sharePanel.SetActive(true);
+    }
+}
